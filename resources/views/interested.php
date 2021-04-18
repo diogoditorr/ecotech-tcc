c15743b6e62f8e54851899a7fc8b5e341e11c9ce
DiegoGamesFight <35296262+diogoditorr@users.noreply.github.com>
2021-04-18 15:17:25 -0300

    Página de Perfil - Interessados

---
+++ resources/views/interested.php
@@ -0,0 +1,92 @@
+<?php
+    $title = 'Ecotech | Interessados';
+    $css['locations'] = [
+        '../../public/styles/page-interested.css',
+        '../../public/styles/navigation-bar.css',
+        '../../public/styles/navigation-profile.css',
+        '../../public/styles/animations.css',
+    ];
+
+    include('../layouts/header.php');
+?>
+
+<div id="page-interested">
+    <nav>
+        <div class="left">
+            <img class="logo" src="../../public/assets/logo.svg" alt="">
+
+            <div class="tabs">
+                <a href="./explore.php">Explore</a>
+                <a class="active" href="#">Profile</a>
+            </div>
+        </div>
+
+        <div class="right">
+            <div class="profile">
+                <img class="user-icon" src="../../public/assets/user-icon.svg" alt="User Icon">
+                <div class="name">Diego</div>
+                <img class="dropdown" src="../../public/assets/dropdown.svg" alt="dropdown">
+            </div>
+        </div>
+    </nav>
+
+    <main>
+        <section class="navigation-profile">
+            <a href="" class="active">
+                <?php echo file_get_contents("../../public/assets/heart.svg"); ?>
+                <span>Interessados</span>
+            </a>
+            <a href="">
+                <?php echo file_get_contents("../../public/assets/order.svg"); ?>
+                <span>Pedidos</span>
+            </a>
+            <a href="">
+                <?php echo file_get_contents("../../public/assets/donation.svg"); ?>
+                <span>Doações</span>
+            </a>
+        </section>
+
+        <section class="content">
+            <header>
+                <p>Interessados</p>
+            </header>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Nome</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="image"><img src="../../storage/parts/image_1.png" alt=""></td>
+                        <td class="name">Semicondutores e transístores</td>
+                        <td class="see-details">
+                            <a href="">
+                                <img src="../../public/assets/info.svg" alt="">
+                                <span>Detalhes</span>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="image"><img src="../../storage/parts/image_1.png" alt=""></td>
+                        <td class="name">Semicondutores e transístores e transístores e transístores e transístores</td>
+                        <td class="see-details">
+                            <a href="">
+                                <img src="../../public/assets/info.svg" alt="">
+                                <span>Detalhes</span>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    </main>
+    
+
+</div>
+
+
+<?php include('../layouts/footer.php'); ?>

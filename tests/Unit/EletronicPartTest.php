a22e400a2460bffc929edc66f43ff9e06833db84
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2022-06-05 00:05:44 -0300

    Add interested and update eletronic part tests

--- tests/Unit/EletronicPartTest.php
+++ tests/Unit/EletronicPartTest.php
@@ -1,10 +1,13 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Tests\Unit;
 
 
 use App\Controllers\EletronicPartsController;
 use App\Controllers\PeopleController;
+use App\Models\EletronicPart;
 use PHPUnit\Framework\TestCase;
 
 final class EletronicPartTest extends TestCase
@@ -29,7 +32,14 @@ final class EletronicPartTest extends TestCase
         }
     }
 
-    public function testGetAllEletronicParts()
+    public function testGetFirst()
+    {
+        $eletronicPart = EletronicPartsController::getFirst();
+
+        $this->assertTrue($eletronicPart instanceof EletronicPart);
+    }
+
+    public function testGetAll()
     {
         $eletronicParts = EletronicPartsController::getAll();
 
@@ -55,3 +65,108 @@ final class EletronicPartTest extends TestCase
 
         $this->assertEquals(count($eletronicPartsOnMock), count($eletronicPartsOnDatabase));
     }
+
+    public function testGetAllByIds()
+    {
+        $eletronicParts = EletronicPartsController::getAll();
+        $filteredEletronicParts = EletronicPartsController::getAllByIds([
+            $eletronicParts[0]->getId(),
+            $eletronicParts[1]->getId(),
+        ]);
+
+        $this->assertCount(2, $filteredEletronicParts);
+    }
+
+    public function testGetById()
+    {
+        $eletronicPart = EletronicPartsController::getFirst();
+        $filteredEletronicPart = EletronicPartsController::getById($eletronicPart->getId());
+
+        $this->assertEquals($eletronicPart->getId(), $filteredEletronicPart->getId());
+    }
+
+    public function testEdit()
+    {
+        $eletronicPart = EletronicPartsController::getFirst();
+
+        $eletronicPart->setName('Computador Ultra Rápido');
+
+        $result = EletronicPartsController::edit([
+            'id' => $eletronicPart->getId(),
+            'userId' => $eletronicPart->getPersonId(),
+            'name' => $eletronicPart->getName(),
+            'type' => $eletronicPart->getType(),
+            'model' => $eletronicPart->getModel(),
+            'description' => $eletronicPart->getDescription(),
+            'stock' => $eletronicPart->getStock(),
+            'image' => null,
+        ]);
+
+        $this->assertTrue($result['success'], (string) $result['error']);
+    }
+
+    public function testUpdateStock()
+    {
+        $eletronicPart = EletronicPartsController::getFirst();
+
+        $result = EletronicPartsController::updateStock($eletronicPart->getId(), $eletronicPart->getStock() + 1);
+
+        $this->assertTrue($result);
+    }
+
+    public function testDelete()
+    {
+        $eletronicPart = EletronicPartsController::getFirst();
+
+        $result = EletronicPartsController::delete($eletronicPart->getId());
+
+        $this->assertTrue($result);
+        $this->assertNull(EletronicPartsController::getById($eletronicPart->getId()));
+    }
+
+    public function testErrorOnImageFormat()
+    {
+        $result1 = EletronicPartsController::register([
+            'image' => null
+        ]);
+
+        $result2 = EletronicPartsController::register([
+            'image' => []
+        ]);
+
+        $result3 = EletronicPartsController::register([
+            'image' => '123'
+        ]);
+
+        $this->assertFalse($result1['success']);
+        $this->assertFalse($result2['success']);
+        $this->assertFalse($result3['success']);
+    }
+
+    public function testInvalidateIncorrectImageExtension()
+    {
+        $image = EletronicPartsController::formatImage([
+            'name' => 'test.php',
+            'type' => 'image/php',
+            'tmp_name' => __DIR__ . '/../Data/test.php',
+        ]);
+
+        $result = EletronicPartsController::register([
+            'image' => $image,
+        ]);
+
+        $this->assertFalse($result['success'], (string) $result['error']);
+
+        $image = EletronicPartsController::formatImage([
+            'name' => 'test.txt',
+            'type' => 'image/text',
+            'tmp_name' => __DIR__ . '/../Data/test.txt',
+        ]);
+
+        $result = EletronicPartsController::register([
+            'image' => $image,
+        ]);
+
+        $this->assertFalse($result['success'], (string) $result['error']);
+    }
+}
---
+++ tests/Unit/InterestedTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use App\Controllers\EletronicPartsController;
+use App\Controllers\InterestedController;
+use App\Controllers\PeopleController;
+use App\Models\EletronicPart;
+use App\Models\Profile;
+use PHPUnit\Framework\TestCase;
+
+final class InterestedTest extends TestCase
+{
+    private static array $fakeData;
+    private static Profile $profile;
+    private static EletronicPart $eletronicPart;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$fakeData = require __DIR__ . '/../Data/fakedata.php';
+        self::$profile = PeopleController::loadProfile(cpf: self::$fakeData['people'][0]['cpf']);
+        self::$eletronicPart = EletronicPartsController::getFirst();
+    }
+
+    public function testIsEletronicPartFavorited()
+    {
+        $result = InterestedController::isEletronicPartFavorited(
+            self::$eletronicPart->getId(),
+            self::$profile->getPersonId()
+        );
+
+        $this->assertFalse($result);
+    }
+
+    public function testFavoriteEletronicPart()
+    {
+        $result = InterestedController::favoriteEletronicPart(
+            self::$eletronicPart->getId(),
+            self::$profile->getPersonId()
+        );
+
+        $this->assertTrue($result);
+        $this->assertTrue(
+            InterestedController::isEletronicPartFavorited(
+                self::$eletronicPart->getId(),
+                self::$profile->getPersonId()
+            )
+        );
+    }
+
+    public function testUnfavoriteEletronicPart()
+    {
+        InterestedController::favoriteEletronicPart(
+            self::$eletronicPart->getId(),
+            self::$profile->getPersonId()
+        );
+        $result = InterestedController::unfavoriteEletronicPart(
+            self::$eletronicPart->getId(),
+            self::$profile->getPersonId()
+        );
+
+        $this->assertTrue($result);
+        $this->assertFalse(
+            InterestedController::isEletronicPartFavorited(
+                self::$eletronicPart->getId(),
+                self::$profile->getPersonId()
+            )
+        );
+    }
+}
